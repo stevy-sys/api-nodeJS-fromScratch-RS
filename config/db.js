80fb9e43c390568924361bc19a726d43207c137d
stevy <stevyralambomanana@gmail.com>
2021-06-01 20:31:32 +0200

    first commit

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.DB_CONF, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
